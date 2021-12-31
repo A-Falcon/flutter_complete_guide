0da024e42c298eae678064ecd70437b9c97e75f9
Ahtziri <Afalcon1209@gmail.com>
2021-12-31 13:55:26 -0800

    add scores and resultPhrases

--- lib/main.dart
+++ lib/main.dart
@@ -18,20 +18,37 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': 'what\'s your favorite color?',
-      'answers': ['Black', 'Red', 'Green', 'White'],
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
     },
     {
       'questionText': 'what\'s your favorite animal?',
-      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+      ],
     },
     {
       'questionText': 'who\'s your favorite instructor?',
-      'answers': ['Me', 'Me', 'Me', 'Me'],
+      'answers': [
+        {'text': 'Me', 'score': 1},
+        {'text': 'Me', 'score': 1},
+        {'text': 'Me', 'score': 1},
+        {'text': 'Me', 'score': 1},
+      ],
     },
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -60,7 +77,7 @@ class _MyAppState extends State<MyApp> {
                   answerQuestion: _answerQuestion,
                   questionIndex: _questionIndex,
                   questions: _questions)
-              : Result()),
+              : Result(_totalScore)),
     );
   }
 }
